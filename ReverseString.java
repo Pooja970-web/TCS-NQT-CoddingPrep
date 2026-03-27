b4b24f64cbd70c7566b67cc1266f4d1734ad4f4c
Pooja970-web <poojklh15@gmail.com>
2026-03-27 17:38:16 +0530

    ReverseString

---
+++ ReverseString.java
@@ -0,0 +1,10 @@
+public class ReverseString{
+  public static void main(String[] args) {
+    String s="Pooja";
+    String reverse="";
+    for(int i=s.length()-1;i>=0;i--){
+      reverse=reverse+s.charAt(i);
+    }
+    System.out.println(reverse);
+  }
+}
