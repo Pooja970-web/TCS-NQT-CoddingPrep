4adbaa4983988e9df063e2359658ef41d6871103
Pooja970-web <poojklh15@gmail.com>
2026-03-27 18:53:47 +0530

    GCDOFTwoNumbers

---
+++ GCDOfTwoNumbers.java
@@ -0,0 +1,11 @@
+public class GCDOfTwoNumbers{
+  public static void main(String[] args) {
+    int a=12,b=15,gcd=1;
+    for(int i=1;i<=a && i<=b;i++){
+      if(a%i==0 && b%i==0){
+        gcd=i;
+      }
+    }
+    System.out.println(gcd);
+  }
+}
