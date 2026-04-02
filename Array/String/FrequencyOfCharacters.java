d56d951b7ec17fa86e7e371ae1ea5ff404d137e7
Pooja970-web <poojklh15@gmail.com>
2026-04-02 21:49:51 +0530

    .

--- Array/String/FrequencyOfCharacters.java
+++ Array/String/FrequencyOfCharacters.java
@@ -3,22 +3,18 @@ import java.util.*;
 public class FrequencyOfCharacters {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Enter the string to find the frequency of characters:");
         String str = sc.nextLine();
-
+        int count;
         for(int i = 0; i < str.length(); i++){
-            int count = 1;
-
-            if(str.charAt(i) == '*') continue;
-
-            for(int j = i + 1; j < str.length(); j++){
+            count = 0;
+            for(int j =1; j <str.length(); j++){
                 if(str.charAt(i) == str.charAt(j)){
                     count++;
-                    str = str.substring(0,j) + '*' + str.substring(j+1);
                 }
             }
 
-            System.out.println(str.charAt(i) + " -> " + count);
+            System.out.println("Frequency of " + str.charAt(i) + " is: " + count);
         }
     }
 }
